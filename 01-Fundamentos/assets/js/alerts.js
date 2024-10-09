79fae3154f290a6f22951a2ea6245729d10c9377
Oficina <carloscarrasco@cev.com>
2024-10-09 17:29:26 +0200

    first commit

---
+++ 01-Fundamentos/assets/js/alerts.js
@@ -0,0 +1,10 @@
+
+
+
+//alert('Hola mundo!');
+
+// let respuesta = prompt('Qué marca de cerveza te mola?', 'Mahou');
+// console.log('La cerveza que te gusta es... ' + respuesta);
+
+const seleccion = confirm('¿Te parece todo ok?');
+console.log(seleccion);
---
+++ 01-Fundamentos/assets/js/app.js
@@ -0,0 +1,21 @@
+
+// console.log(console.log('Hola mundo'));
+let a = 10,
+    b = 21,
+    c = 'Hola ',
+    d = 'Deadpool',
+    x = a + b;
+
+const saludo = c + d;
+// console.log('%c Mis variables', 'color:blue; font-weight: bold');
+// console.log({ a });
+// console.log({ b });
+// console.log({ c });
+
+
+c = 'Hola holita vecinito';
+
+// console.table({ a, b, c, d, x });
+
+
+var miNuevoNombre = 'Carlitos';
